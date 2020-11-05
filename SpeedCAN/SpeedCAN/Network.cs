9c21ea0add9bdc4339ad2f3682ac3d517c605248
Luca <luca@retina-bv.nl>
2020-11-05 14:19:20 +0100

    Added project files

---
+++ SpeedCAN/SpeedCAN/Network.cs
@@ -0,0 +1,125 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Network;
+using GHIElectronics.TinyCLR.Pins;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace SpeedCAN
+{
+    public class Network
+    {
+        string IP;
+        string SubnetMask;
+        string Gateway;
+        string DNS;
+        byte[] MacAddress;
+
+        private readonly GpioPin chipSelectPin;
+        private readonly GpioPin ethResetPin;
+        private readonly GpioPin ethInterruptPin;
+
+        private string SpiAPI;
+
+        public bool HasLink { get; set; }
+
+        public Network(string ip, string subnetmask, string gateway, string dns, byte[] mac, int chipSelect = SC20100.GpioPin.PB8, int ethReset = SC20100.GpioPin.PB9, int ethInterrupt = SC20100.GpioPin.PD6, string spi = SC20260.SpiBus.Spi3)
+        {
+            IP = ip;
+            SubnetMask = subnetmask;
+            Gateway = gateway;
+            DNS = dns;
+            MacAddress = mac;
+
+            chipSelectPin = GpioController.GetDefault().OpenPin(chipSelect);
+            ethResetPin = GpioController.GetDefault().OpenPin(ethReset);
+            ethInterruptPin = GpioController.GetDefault().OpenPin(ethInterrupt);
+
+            SpiAPI = spi;
+        }
+
+        public void InitializeNetwork()
+        {
+            var networkController = NetworkController.FromName("GHIElectronics.TinyCLR.NativeApis.ENC28J60.NetworkController");
+
+            var networkInterfaceSetting = new EthernetNetworkInterfaceSettings();
+
+            var networkCommunicationInterfaceSettings = new SpiNetworkCommunicationInterfaceSettings();
+
+            var settings = new GHIElectronics.TinyCLR.Devices.Spi.SpiConnectionSettings()
+            {
+                ChipSelectLine = chipSelectPin,
+                ClockFrequency = 4000000,
+                Mode = GHIElectronics.TinyCLR.Devices.Spi.SpiMode.Mode0,
+                ChipSelectType = GHIElectronics.TinyCLR.Devices.Spi.SpiChipSelectType.Gpio,
+                ChipSelectHoldTime = TimeSpan.FromTicks(10),
+                ChipSelectSetupTime = TimeSpan.FromTicks(10)
+            };
+
+            networkCommunicationInterfaceSettings.SpiApiName = SpiAPI;
+            networkCommunicationInterfaceSettings.GpioApiName = SC20100.GpioPin.Id;
+            networkCommunicationInterfaceSettings.SpiSettings = settings;
+
+            networkCommunicationInterfaceSettings.InterruptPin = ethInterruptPin;
+            networkCommunicationInterfaceSettings.InterruptEdge = GpioPinEdge.FallingEdge;
+            networkCommunicationInterfaceSettings.InterruptDriveMode = GpioPinDriveMode.InputPullUp;
+
+            networkCommunicationInterfaceSettings.ResetPin = ethResetPin;
+            networkCommunicationInterfaceSettings.ResetActiveState = GpioPinValue.Low;
+
+            networkInterfaceSetting.Address = new IPAddress(StringToIP(IP));
+            networkInterfaceSetting.SubnetMask = new IPAddress(StringToIP(SubnetMask));
+            networkInterfaceSetting.GatewayAddress = new IPAddress(StringToIP(Gateway));
+            networkInterfaceSetting.DnsAddresses = new IPAddress[] { new IPAddress(StringToIP(DNS)) };
+
+            networkInterfaceSetting.MacAddress = MacAddress;
+            networkInterfaceSetting.IsDhcpEnabled = false;
+
+            networkController.SetInterfaceSettings(networkInterfaceSetting);
+            networkController.SetCommunicationInterfaceSettings(networkCommunicationInterfaceSettings);
+
+            networkController.SetAsDefaultController();
+
+            networkController.NetworkAddressChanged += NetworkController_NetworkAddressChanged;
+            networkController.NetworkLinkConnectedChanged += NetworkController_NetworkLinkConnectedChanged;
+
+            networkController.Enable();
+
+            //Wait until we get a link
+            while (!HasLink)
+            {
+                Debug.WriteLine("Waiting for network link...");
+                Thread.Sleep(1000);
+            }
+
+            Debug.WriteLine("Network link achieved!");
+        }
+        private void NetworkController_NetworkLinkConnectedChanged(NetworkController sender, NetworkLinkConnectedChangedEventArgs e)
+        {
+        }
+
+        private void NetworkController_NetworkAddressChanged(NetworkController sender, NetworkAddressChangedEventArgs e)
+        {
+            var ipProperties = sender.GetIPProperties();
+            var address = ipProperties.Address.GetAddressBytes();
+
+            HasLink = address[0] != 0;
+        }
+
+        private static byte[] StringToIP(string stringIP)
+        {
+            byte[] IP = new byte[4];
+            string[] splitIP = stringIP.Split('.');
+
+            for (int i = 0; i < 4; i++)
+                IP[i] = byte.Parse(splitIP[i]);
+
+            return IP;
+        }
+
+
+    }
+}
---
+++ SpeedCAN/SpeedCAN/Program.cs
@@ -0,0 +1,79 @@
+﻿using GHIElectronics.TinyCLR.Devices.Can;
+using GHIElectronics.TinyCLR.Pins;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SpeedCAN
+{
+    class Program
+    {
+        public static Socket sock;
+        public static CanController can;
+        public static Network Network;
+
+        public static IPAddress serverAddr = IPAddress.Parse("192.168.181.209");
+
+        public static IPEndPoint endPoint = new IPEndPoint(serverAddr, 5051);
+
+        public static CanMessage msg;
+        public static int msgCount = 0;
+
+        static void Main()
+        {
+            //Instantiate the network class
+            Network = new Network("192.168.181.210", "255.255.255.0", "192.168.181.1", "192.168.181.1", new byte[] { 0xA1, 0xA6, 0xB9, 0x3E, 0xD0, 0x1F });
+
+            //Initialize the network
+            Network.InitializeNetwork();
+
+            //Create a UDP socket
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //Start the CAN at 250kbps
+            can = CanController.FromName(SC20100.CanBus.Can1);
+            can.SetNominalBitTiming(new CanBitTiming(13, 2, 12, 1, false));
+            can.ErrorReceived += Can_ErrorReceived; //Attach to the error event
+
+            can.Enable();//Enable the CAN peripheral
+
+            new Thread(Print).Start(); //Start a thread that will print how many messages we processed last second
+
+            //A byte array used in the loop
+            byte[] data = new byte[8];
+
+            while (true)
+            {
+                //If we have messages to read
+                if (can.MessagesToRead > 0)
+                {
+                    //Read the message
+                    can.ReadMessage(out msg);
+                    msgCount++; //Increment the count
+                    Array.Copy(msg.Data, data, 8); //Copy the data so we only take the first 8 received bytes instead of all 64
+                    //sock.SendTo(data, endPoint); //Send the data to the endpoint over the socket. REMOVE THIS LINE TO BE ABLE TO PROCESS AS MUCH MESSAGES AS CAN ALLOWS
+                }
+            }
+        }
+
+        //A static function that runs as a seperate thread
+        //It prints the amount of messages every second and resets the counter
+        public static void Print()
+        {
+            while(true)
+            {
+                Debug.WriteLine(msgCount.ToString());
+                msgCount = 0;
+                Thread.Sleep(1000);
+            }
+        }
+
+        //Triggered when an error on the CAN bus occurs
+        private static void Can_ErrorReceived(CanController sender, ErrorReceivedEventArgs e)
+        => System.Diagnostics.Debug.WriteLine("error " + ((object)e.Error).ToString());
+    }
+}
